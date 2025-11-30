520775412d37f7610091255b7e08c803747c16ee
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-11-30 11:23:10 +0100

    Implemented data parsing. Added TODOs.

---
+++ Main.R
@@ -0,0 +1,17 @@
+# install.packages("XLConnect")
+library(XLConnect)
+
+# reads IBI values from XLSX (one sheet, one column, no header)
+parse_ibi <- function(path) {
+  ibi <- readWorksheet(loadWorkbook(path),sheet=1, header=FALSE)
+  colnames(ibi) <- c("IBI")
+  return(ibi)
+}
+
+# parse IBI data
+ibi_a <- parse_ibi("HRV_a.xlsx")
+ibi_b <- parse_ibi("HRV_b.xlsx")
+
+# TODO: calculate summary stats for IBI series
+
+# TODO: plot IBI series
